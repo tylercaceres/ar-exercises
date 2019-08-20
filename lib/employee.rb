8ebe249ac7366f512208a5530bb96fc1025e23d8
tylerccaceres <tyler.caceres@gmail.com>
2019-08-20 09:15:14 -0400

    final touches #still confused

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
-class Employee < ActiveRecord::Base
+# frozen_string_literal: true
 
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, presence: true, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store_id, presence: true, allow_nil: false, allow_blank: false
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
-class Store < ActiveRecord::Base
+# frozen_string_literal: true
 
+class Store < ActiveRecord::Base
+  has_many :employees
+  validates :annual_revenue, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true, length: { minimum: 3 }
+  # validate :mens_or_womens_apparel
+
+  # def mens_or_womens_apparel
+  #   if mens_apparel == false && womens_apparel == false
+  #     errors.add(:name, "must carry men's or women's apparel")
+  #   end
+  # end
 end
